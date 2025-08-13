f0ff945924d1b774c315c585f965e09153125814
paranengmoose <paranengmoose@gmail.com>
2025-08-12 21:06:17 -0400

    MSE219 Chapter 2-3

--- src/data/catalog.js
+++ src/data/catalog.js
@@ -15,6 +15,7 @@ import mse219_ch1_3 from './chapters/mse219_chapter1_3.js'
 import mse219_ch1_4 from './chapters/mse219_chapter1_4.js'
 import mse219_ch2_1 from './chapters/mse219_chapter2_1.js'
 import mse219_ch2_2 from './chapters/mse219_chapter2_2.js'
+import mse219_ch2_3 from './chapters/mse219_chapter2_3.js'
 
 export const CATALOG = {
   MSE120: {
@@ -34,7 +35,8 @@ export const CATALOG = {
       mse219_ch1_3,
       mse219_ch1_4,
       mse219_ch2_1,
-      mse219_ch2_2
+      mse219_ch2_2,
+      mse219_ch2_3
     ],
   },
   MSE316: {
