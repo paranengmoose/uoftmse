4c6d04c7c3f2c8ba85363bc11d3f7acc20eb9592
paranengmoose <paranengmoose@gmail.com>
2025-08-12 20:12:33 -0400

    MSE219 Chapter 1-3

--- src/data/catalog.js
+++ src/data/catalog.js
@@ -11,6 +11,7 @@ import mse335_ch9 from './chapters/mse335_chapter9.js'
 import mse335_ch10 from './chapters/mse335_chapter10.js'
 import mse219_ch1_1 from './chapters/mse219_chapter1_1.js'
 import mse219_ch1_2 from './chapters/mse219_chapter1_2.js'
+import mse219_ch1_3 from './chapters/mse219_chapter1_3.js'
 
 export const CATALOG = {
   MSE120: {
@@ -26,7 +27,8 @@ export const CATALOG = {
     name: "MSE 219 – Structure and Characterization of Materials",
     chapters: [
       mse219_ch1_1,
-      mse219_ch1_2
+      mse219_ch1_2,
+      mse219_ch1_3
     ],
   },
   MSE316: {
